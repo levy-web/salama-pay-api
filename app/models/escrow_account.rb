77fc9cf59339cc272be1094a482d71c8f7f60941
levy <lnjiru43@gmail.com>
2023-09-19 22:53:13 +0300

    subtract and add to escrow account balance

--- app/models/escrow_account.rb
+++ app/models/escrow_account.rb
@@ -1,3 +1,13 @@
 class EscrowAccount < ApplicationRecord
     has_many :transactions, class_name: 'Transaction', foreign_key: 'escrow_account_id'
+
+    def subtract_from_escrow_account_balance(amount)
+        self.balance -= amount
+        save
+    end
+
+    def add_to_escrow_account_balance(amount)
+        self.balance += amount
+        save
+    end
 end
