e9ac6a5a26aa57e6d0a2caa2d796c54397601571
levy <lnjiru43@gmail.com>
2023-09-19 22:52:38 +0300

    subtract and add to balance method

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,13 @@
 class Account < ApplicationRecord
     belongs_to :user
+
+    def subtract_from_personal_account_balance(amount)
+        self.balance -= amount
+        save
+    end
+
+    def add_to_personal_account_balance(amount)
+        self.balance += amount
+        save
+    end
 end
