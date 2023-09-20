f7e2d12ce39e4e2277cef5d4e9625a098852af0a
levy <lnjiru43@gmail.com>
2023-09-20 17:09:08 +0300

    added pending buyer for the seller to see transactions that have not been completed yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
     has_one :account
+    has_one :held_fund
     has_many :sent_transactions, class_name: 'Transaction', foreign_key: 'user_id'
     has_many :received_transactions, class_name: 'Transaction', foreign_key: 'opposite_user_id'
-    has_many :pending_seller_transactions
+    has_many :pending_buyer_transactions, class_name: 'PendingSellerTransaction', foreign_key: 'user_id'
+    has_many :pending_seller_transactions, class_name: 'PendingSellerTransaction', foreign_key: 'opposite_user_id'
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,6 +3,7 @@ class UserSerializer < ActiveModel::Serializer
   has_many :sent_transactions
   has_many :received_transactions
   has_many :pending_seller_transactions
+  has_many :pending_buyer_transactions
 
-  attributes :id, :firstName, :middleName, :surname
+  attributes :id, :firstName, :middleName, :surname, :account
 end
