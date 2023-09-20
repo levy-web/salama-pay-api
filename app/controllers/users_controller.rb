5993086eea4b71cb86e3f23a0d2cf271a6b25667
levy <lnjiru43@gmail.com>
2023-09-20 19:49:33 +0300

    added user routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,57 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: %i[ show update destroy ]
+  
+    # GET /users
     def index
-        render json: User.all
+      @users = User.all
+  
+      render json: @users
     end
-end
+  
+    # GET /users/1
+    def show
+      render json: @user
+    end
+  
+   # POST /users
+    def create
+        @user = User.new(user_params)
+    
+        if @user.save
+        # Create a new account for the user with a balance of 5000
+        Account.create(user: @user, balance: 0)
+        HeldFund.create(amount: 0,user: @user)
+    
+        render json: @user, status: :created
+        else
+        render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+  
+  
+  
+    # PATCH/PUT /users/1
+    def update
+      if @user.update(user_params)
+        render json: @user
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /users/1
+    def destroy
+      @user.destroy
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_user
+        @user = User.find(params[:id])
+      end
+  
+      # Only allow a list of trusted parameters through.
+      def user_params
+        params.permit(:firstName, :middleName, :surname, :email, :password, :address, :phone)
+      end
+  end
