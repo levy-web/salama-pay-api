ef2286a0ac70a44a5d6bf8e41faa28578ebdf5da
levy <lnjiru43@gmail.com>
2023-09-20 19:50:09 +0300

    jwt encode method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+
+    def encode_token(uid, email, password)
+        payload = {
+            data:{
+                uid: uid,
+                email: email,
+                secret: password
+            },
+            exp: Time.now.to_i + (0.5*3600)
+        }
+        JWT.encode(payload, ENV['salama_key'], 'HS256')
+    end
+
 end
