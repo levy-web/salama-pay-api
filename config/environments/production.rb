73c71e23c5582c27b736427e44fc3f07a7fdfafd
levy <lnjiru43@gmail.com>
2023-09-27 00:55:04 +0300

    smtp setup app password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,8 +90,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: 'lnjiru43@gmail.com',
-    password: 'Passweed12345',
+    user_name: 'levymuguro@gmail.com',
+    password: 'wnao glil bmoq mcnp',
     authentication: 'plain',
     enable_starttls_auto: true
   }
