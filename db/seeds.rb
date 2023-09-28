6e3bb4ef6a2fae756f62c6b8ebef36a29d933874
levy <lnjiru43@gmail.com>
2023-09-28 09:35:47 +0300

    seed data of escrow account

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Escrow_account.create(balance: 20000.00)
