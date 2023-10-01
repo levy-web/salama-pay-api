b03d4bb2c99c2ab1c87ef24d68966b5bf2ac3ee8
Preston <prestonosoro56@gmail.com>
2023-10-01 17:56:49 +0300

    feat: add send_email method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,16 @@
 class UserMailer < ApplicationMailer
-    def send_verification_code(user)
-        @user = user
-        mail(to: @user.email, subject: 'Salama Verification Code')
+  def send_verification_code(user)
+    @user = user
+    mail(to: @user.email, subject: "Salama Verification Code")
+  end
+
+  def send_email(email)
+    @transaction_id = email.transaction_id
+    @issue_details = email.issue_details
+    @dispute_details = email.dispute_details
+
+    mail(to: "recipient@gmail.com", subject: "New Help Request") do |format|
+      format.text { render plain: "This is the plain text body of the email." }
     end
+  end
 end
