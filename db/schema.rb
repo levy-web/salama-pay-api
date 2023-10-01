60bfe00c210256860741dcd3403f555e50a146bc
Preston <prestonosoro56@gmail.com>
2023-10-01 17:59:21 +0300

    feat: emails table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_171421) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_01_123010) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_171421) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "emails", force: :cascade do |t|
+    t.string "transaction_id"
+    t.text "issue_details"
+    t.text "dispute_details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "escrow_accounts", force: :cascade do |t|
